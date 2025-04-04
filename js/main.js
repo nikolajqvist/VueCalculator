58674650f80598c42eb306e56a54b2b154613bd3
Nikolaj Qvist <nqvistmoller@hotmail.com>
2025-04-04 11:20:20 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data () {
+        return{
+            intro: 'Velkommen til min Vue Template'
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+})
