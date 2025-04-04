14bdae45201effafb97e852c441e37af65077654
Nikolaj Qvist <nqvistmoller@hotmail.com>
2025-04-04 12:15:13 +0200

    calculator done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 const app = Vue.createApp({
     data () {
         return{
-            intro: 'Velkommen til min Vue Template'
+            intro: 'Velkommen til min Vue lommeregner',
+            number1: '',
+            number2: '',
+            result: '',
+            operation: '',
         }
     },
     methods: {
-
+        calculate() {
+            switch(this.operation){
+            case '+': return this.result += this.number1 + this.number2;
+            case '-': return this.result += this.number1 - this.number2;
+            case '*': return this.result += this.number1 * this.number2;
+            case '/': return this.result += this.number1 / this.number2;
+        }
+    }
     },
     computed: {
 
